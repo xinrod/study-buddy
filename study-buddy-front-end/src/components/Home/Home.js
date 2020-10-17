9407c0551a3865b0d274ebf981c04b87adccebc3
Roderick Liu <xinrod@live.unc.edu>
2020-10-17 13:42:21 -0400

    home page

--- study-buddy-front-end/src/components/Home/Home.js
+++ study-buddy-front-end/src/components/Home/Home.js
@@ -1,11 +1,19 @@
 import React from 'react';
+import './Home.css';
+import {Link} from 'react-router-dom';
 
 const Home = () => {
 
-    return(
+    return (
         <>
-            <nav>
-                THIS IS THE HOME PAGE
+            <nav className="dt w-100 border-box pa3 ph5-ns">
+                <Link path='/' className="link dim dark-gray f6 f5-ns dib mr3 mr4-ns" href="#" title="Home">
+                    <img src="http://tachyons.io/img/logo.jpg" className="dib w2 h2 br-100" alt="Site Name" />
+                    Buddy Study
+                </Link>
+                <div className="dtc v-mid w-75 tr">
+                    <a className="link dim dark-gray f6 f5-ns dib" href="#" title="Contact">Sign Out</a>
+                </div>
             </nav>
         </>
     );
