aca54d8914cf1f4ebfd51aa735973ed0a2da4720
Roderick Liu <xinrod@live.unc.edu>
2020-10-17 19:27:03 -0400

    finished back-end for home

--- back-end/app.js
+++ back-end/app.js
@@ -3,9 +3,12 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
-
-var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
+const bodyParser = require('body-parser');
+const MongoClient = require('mongodb').MongoClient
+
+const uri = "mongodb+srv://admin:admin@cluster0.wynk5.mongodb.net/studyBuddy?retryWrites=true&w=majority";
+const client = new MongoClient(uri, { useUnifiedTopology: true });
 
 var app = express();
 
@@ -15,20 +18,65 @@ app.set('view engine', 'ejs');
 
 app.use(logger('dev'));
 app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
+app.use(bodyParser.urlencoded({ extended: true }))
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', indexRouter);
+// '/' path
+app.post('/', async (req, res) => {
+  if (req.body.id === 'classes') {
+    return res.status(500).send(error);
+  }
+  console.log(req.body);
+  await client.connect();
+  const datab = client.db('studyBuddy')
+  datab.createCollection(req.body.id);
+  const collection = datab.collection('classes');
+  collection.insertOne(req.body, (error, result) => {
+    if (error) { return res.status(500).send(error); }
+    res.send(result.result);
+  })
+});
+
+app.get('/', async (req, res) => {
+  console.log(req.body);
+  await client.connect();
+  const collection = client.db('studyBuddy').collection('classes');
+  collection.find({}).toArray((error, result) => {
+    if (error) {
+      return response.status(500).send(error);
+    }
+    res.send(result);
+  });
+});
+
+app.delete('/', async (req, res) => {
+  if (req.body.id === 'classes') {
+    return res.status(500).send(error);
+  }
+  console.log(req.body.name);
+  await client.connect();
+  const database = client.db('studyBuddy');
+  database.collection(req.body.id).drop();
+  const collection = database.collection('classes');
+  collection.deleteOne(({name : req.body.name}, {id : req.body.id}), function(err, result) {
+    if (err) throw err;
+    console.log("1 document deleted");
+    res.send(result);
+  });
+});
+
+
+
 app.use('/users', usersRouter);
 
 // catch 404 and forward to error handler
-app.use(function(req, res, next) {
+app.use(function (req, res, next) {
   next(createError(404));
 });
 
 // error handler
-app.use(function(err, req, res, next) {
+app.use(function (err, req, res, next) {
   // set locals, only providing error in development
   res.locals.message = err.message;
   res.locals.error = req.app.get('env') === 'development' ? err : {};
