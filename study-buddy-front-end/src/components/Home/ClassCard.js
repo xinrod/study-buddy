8597184ea15ba8390536873cdb9ff557a136dcfa
Roderick Liu <xinrod@live.unc.edu>
2020-10-17 13:50:35 -0400

    added some components

---
+++ study-buddy-front-end/src/components/Home/ClassCard.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './Home.css';
+
+const ClassCard = () => {
+
+    return (
+        <>
+            
+            
+        </>
+    );
+}
+
+export default ClassCard;
--- study-buddy-front-end/src/components/Home/Home.js
+++ study-buddy-front-end/src/components/Home/Home.js
@@ -7,14 +7,16 @@ const Home = () => {
     return (
         <>
             <nav className="dt w-100 border-box pa3 ph5-ns">
-                <Link path='/' className="link dim dark-gray f6 f5-ns dib mr3 mr4-ns" href="#" title="Home">
+                <Link path='/' className="link dim dark-gray f6 f5-ns dib mr3 mr4-ns" title="Home">
                     <img src="http://tachyons.io/img/logo.jpg" className="dib w2 h2 br-100" alt="Site Name" />
                     Buddy Study
                 </Link>
                 <div className="dtc v-mid w-75 tr">
-                    <a className="link dim dark-gray f6 f5-ns dib" href="#" title="Contact">Sign Out</a>
+                    <a className="link dim dark-gray f6 f5-ns dib" href="#" title="signout">Sign Out</a>
                 </div>
             </nav>
+            <h1>Dashboard</h1>
+            
         </>
     );
 }
